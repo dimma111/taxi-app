8c46ef147103ed97ccf6fbd59fb43b44cc15ecf4
dimma111 <marinenko96@yandex.ru>
2022-03-26 23:21:45 +0500

    Create hompage component

---
+++ app/components/screens/home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import Layout from "../../layout/Layout";
+
+const Home: FC = () => {
+  return (
+    <Layout title="Order taxi">
+      <div>Home</div>
+    </Layout>
+  );
+};
+
+export default Home;

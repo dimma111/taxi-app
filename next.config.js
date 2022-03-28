91572364984ab979e09555af6364051fe8772018
dimma111 <marinenko96@yandex.ru>
2022-03-28 22:57:45 +0500

    add env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    MAP_KEY: "AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

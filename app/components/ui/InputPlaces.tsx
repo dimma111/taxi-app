85f2255e10392c2209484c239db797e39489d7b6
Dmitriy <marinenko96@yandex.ru>
2022-04-06 14:54:58 +0500

    complete input component

--- app/components/ui/InputPlaces.tsx
+++ app/components/ui/InputPlaces.tsx
@@ -6,9 +6,16 @@ import cn from "classnames";
 interface IInputPlaces {
   cbSuccess: () => void;
   type: "from" | "to";
+  idInput: string;
+  routeData: object;
 }
 
-const InputPlaces: FC<IInputPlaces> = ({ cbSuccess, type }) => {
+const InputPlaces: FC<IInputPlaces> = ({
+  cbSuccess,
+  type,
+  idInput,
+  routeData,
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const setFocus = () => {
@@ -19,69 +26,45 @@ const InputPlaces: FC<IInputPlaces> = ({ cbSuccess, type }) => {
     if (isFrom) setFocus();
   }, [type]);
 
-  const [address, setAddress] = useState("");
-
-  const handleSelect = () => {};
-
   const isFrom = type === "from";
 
   return (
-    <PlacesAutocomplete
-      value={address}
-      onChange={setAddress}
-      onSelect={handleSelect}
-      onError={(err) => console.log("Error: ", err)}
+    <div
+      className={cn("shadow-lg", {
+        "mb-5": isFrom,
+        "mb-3": !isFrom,
+      })}
+      onClick={setFocus}
     >
-      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-        <div
-          className={cn("shadow-lg", {
-            "mb-5": isFrom,
-          })}
-          onClick={setFocus}
-        >
-          <div
-            className="py-4 px-5 bg-white rounded-lg flex items-center"
-            style={
-              suggestions.length
-                ? { borderBottomRightRadius: 0, borderBottomLeftRadius: 0 }
-                : {}
-            }
-          >
-            <FiSearch
-              color={isFrom ? "#ffbc00" : "#615f5d"}
-              className="mr-3"
-              size={26}
-            />
-            <input
-              {...getInputProps({
-                ref: inputRef,
-                placeholder: isFrom ? "Where from?" : "Where to?",
-                className: "outline-0 w-full text-gray-800",
-              })}
-            />
-            {!isFrom && (
-              <div className="absolute right-5 text-sm text-gray-400">
-                {" "}
-                min.
-              </div>
-            )}
+      <div
+        className="py-4 px-5 bg-white rounded-lg flex items-center"
+        // style={
+        //   suggestions.length
+        //     ? { borderBottomRightRadius: 0, borderBottomLeftRadius: 0 }
+        //     : {}
+        // }
+      >
+        <FiSearch
+          color={isFrom ? "#ffbc00" : "#615f5d"}
+          className="mr-3"
+          size={26}
+        />
+        <input
+          id={idInput}
+          ref={inputRef}
+          placeholder={isFrom ? "Откуда?" : "Куда?"}
+          className="outline-0 w-full text-gray-800"
+          style={!isFrom ? { paddingRight: 70 } : {}}
+        />
+        {!isFrom && (
+          <div className="absolute right-5 text-sm text-gray-400 text-right">
+            {" "}
+            <div>{routeData.time ? routeData.time : "мин"}</div>{" "}
+            <div>{routeData.length ? routeData.length : "км"}</div>
           </div>
-
-          <div
-            className={cn(
-              "absolute w-full h-0 overflow-y-auto rounded-b-lg z-10",
-              { "h-48": suggestions.length || loading }
-            )}
-          >
-            {loading && <div className="bg-white p-2">Loading...</div>}
-
-            {
-              suggestions.map((suggestion, idx) => (<div {...getSuggestionItemProps(suggestion, {className: cn('cursor-pointer p-3', {'bg-gray-100': suggestion.active, 'bg-white': !suggestion.active})})}><span>{suggestion.description}</span></div>))
-            }
-          </div>
-        </div>
-      )}
-    </PlacesAutocomplete>
+        )}
+      </div>
+    </div>
   );
 };
 

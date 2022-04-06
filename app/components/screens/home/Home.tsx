6e29e828a9b8d2fc59f93c28e8d282e6a46e84f8
Dmitriy <marinenko96@yandex.ru>
2022-04-06 14:54:35 +0500

    comple homapage component

--- app/components/screens/home/Home.tsx
+++ app/components/screens/home/Home.tsx
@@ -1,15 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Layout from "../../layout/Layout";
 import FormInput from "./FormInput";
 import MainMap from "./MainMap";
 
 const Home: FC = () => {
+  const [routeData, setRouteData] = useState({ length: "", time: "" });
+
   return (
     <Layout title="Order taxi">
-      <MainMap />
+      <MainMap setRouteData={setRouteData} />
 
       <div className="absolute z-10 left-5 w-11/12 bottom-10 input-wrap">
-        <FormInput />
+        <FormInput routeData={routeData} />
       </div>
     </Layout>
   );
